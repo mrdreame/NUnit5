cc64734f33cc2cca7fcd71f84c0df0a761e9ed4b
mrdreame <muhammad.2612@students.amikom.ac.id>
2021-11-06 21:46:59 +0700

    Add project files.

---
+++ TugasLibraryUnitTest/DayTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class DayTest
+    {
+        private Day dTest;
+        [SetUp]
+        public void Init()
+        {
+            dTest = new Day();
+        }
+
+        [Test]
+        public void NameOfDayTest()
+        {
+            Assert.AreEqual("Jum'at", dTest.NameOfDay(5));
+        }
+    }
+}
---
+++ TugasLibraryUnitTest/UserTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class UserTest
+    {
+        private User uTest;
+
+        [SetUp]
+        public void Init()
+        {
+            uTest = new User();
+        }
+
+        [Test]
+        public void TestUserValid()
+        {
+            Assert.AreEqual(true, uTest.IsValidUser("admin", "admin"));
+        }
+    }
+}
